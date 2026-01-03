8b3856b1f8415667b9025ea1c9991d49e27b0fb4
Cesar Cisnero <cesarlgi01@gmail.com>
2026-01-03 20:27:28 -0300

    Initial commit2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { FooterComponent } from './shared/components/footer/footer.component'
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet, FooterComponent],
+    templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'country-app';
+    title = 'country-app'
 }
