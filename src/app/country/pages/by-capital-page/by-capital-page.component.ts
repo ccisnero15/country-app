2d1140808657652b1a650054ff256bfce3bf81e4
Cesar Cisnero <cesarlgi01@gmail.com>
2026-01-15 21:04:18 -0300

    Avance

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,6 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { Component, inject, signal } from '@angular/core'
 import { SearchInputComponent } from '../../components/search-input/search-input.component'
 import { CountryListComponent } from '../../components/country-list/country-list.component'
+import { CountryService } from '../../services/country.service'
+import { Country } from '../../interfaces/country.interface'
 
 @Component({
     selector: 'app-by-capital-page',
@@ -8,7 +10,20 @@ import { CountryListComponent } from '../../components/country-list/country-list
     templateUrl: './by-capital-page.component.html',
 })
 export class ByCapitalPageComponent {
+    countryService = inject(CountryService)
+    isLoading = signal(false)
+    isError = signal<string | null>(null)
+    countries = signal<Country[]>([])
+
     onSearch(value: string) {
-        console.log(value)
+        if (this.isLoading()) return
+
+        this.isLoading.set(true)
+        this.isError.set(null)
+        this.countryService.searchByCapital(value).subscribe((data) => {
+            this.isLoading.set(false)
+            this.countries.set(data)
+            console.log(data)
+        })
     }
 }
